30cad92690dd51ea7cbf2fc8e5fac079d031ff2c
Varik Valefor <varikvalefor@aol.com>
2021-06-09 18:16:57 -0400

    Programs/Ten.hs: Some unnecessary indentation is removed.

--- Programs/Ten.hs
+++ Programs/Ten.hs
@@ -1,14 +1,15 @@
 module Programs.Ten where
-  import Control.Concurrent;
 
-  bigNums :: [Integer];
-  bigNums = map ((10^).(10^)) [1..];
+import Control.Concurrent;
 
-  toJunk :: Eq a => [a] -> [Char];
-  toJunk = foldr (++) [] . map (\a -> "");
-  -- toJunk is used to load bigNums into memory without actually printing
-  -- bigNums; printing bigNums uses a decent bit of processing power
-  -- without using much RAM, which is undesirable.
+bigNums :: [Integer];
+bigNums = map ((10^).(10^)) [1..];
 
-  main :: IO ();
-  main = putStrLn $ toJunk bigNums;
+toJunk :: Eq a => [a] -> [Char];
+toJunk = foldr (++) [] . map (\a -> "");
+-- toJunk is used to load bigNums into memory without actually printing
+-- bigNums; printing bigNums uses a decent bit of processing power
+-- without using much RAM, which is undesirable.
+
+main :: IO ();
+main = putStrLn $ toJunk bigNums;
