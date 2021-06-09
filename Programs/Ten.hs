1580e7db17bb3fc9e77e1acc52cbcf296b0842d4
Varik Valefor <varikvalefor@aol.com>
2021-06-09 18:26:03 -0400

    Programs/Ten.hs: Some documentation is added.

--- Programs/Ten.hs
+++ Programs/Ten.hs
@@ -2,9 +2,13 @@ module Programs.Ten where
 
 import Control.Concurrent;
 
+-- | bigNums equals <10^10^k | k in N>, where N denotes the sequence of
+-- all natural numbers.
 bigNums :: [Integer];
 bigNums = map ((10^).(10^)) [1..];
 
+-- | g k equals "".  g exists for the purpose of using relatively large
+-- amounts of RAM.
 toJunk :: Eq a => [a] -> [Char];
 toJunk = foldr (++) [] . map (\a -> "");
 -- toJunk is used to load bigNums into memory without actually printing
