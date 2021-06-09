f7320fd29517b16250a965604aeaf38cf98c8c72
Varik Valefor <varikvalefor@aol.com>
2021-06-09 17:51:51 -0400

    Programs/CrapSum.hs: Documentation is added.

--- Programs/CrapSum.hs
+++ Programs/CrapSum.hs
@@ -1,17 +1,23 @@
 module Programs.CrapSum where
-  import Resource.Bullshit;
-  import Control.Monad;
-  import System.Environment;
+import Resource.Bullshit;
+import Control.Monad;
+import System.Environment;
 
-  hexDigit :: [Char];
-  hexDigit = ['0'..'9'] ++ ['a'..'f'];
+-- | hexDigit equals the sequence of all hexadecimal characters, in
+-- ascending order.
+hexDigit :: [Char];
+hexDigit = ['0'..'9'] ++ ['a'..'f'];
 
-  sel :: Int -> IO [Char];
-  sel n = generateCrap >>= return . take n . filter (`elem` hexDigit);
+-- | For all positive Integer k, sel k equals k pseudorandom hexadecimal
+-- characters.
+sel :: Int -> IO [Char];
+sel n = generateCrap >>= return . take n . filter (`elem` hexDigit);
 
-  timeToChoose :: [Char] -> IO ();
-  timeToChoose "crapsum256" = (sel >=> putStrLn) 64;
-  timeToChoose "crapsum512" = (sel >=> putStrLn) 128;
+-- | timeToChoose determines the version of crapsum which should be
+-- used, i.e., the number of junk characters which should be returned.
+timeToChoose :: [Char] -> IO ();
+timeToChoose "crapsum256" = (sel >=> putStrLn) 64;
+timeToChoose "crapsum512" = (sel >=> putStrLn) 128;
 
-  main :: IO ();
-  main = getProgName >>= timeToChoose;
+main :: IO ();
+main = getProgName >>= timeToChoose;
