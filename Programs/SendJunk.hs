ab50f86da1faf79dd8ba77bd04f03b00a91cf5fd
Varik Valefor <varikvalefor@aol.com>
2021-06-09 18:15:27 -0400

    Programs/SendJunk.hs: Some documentation is added.

--- Programs/SendJunk.hs
+++ Programs/SendJunk.hs
@@ -5,11 +5,16 @@ import Resource.PGP;
 import Resource.Bullshit;
 import System.Environment;
 
+-- | For all [String] k, gs k equals a subject line which reflects the
+-- content of k.
 gs :: [String] -> String;
 gs a
   | length a > 2 = a !! 2
   | otherwise = "(URGENT) Patch for Security Vulnerability"
 
+-- | For all String-based PGP public keys k, encCrapWKey generates some
+-- pseudorandom junk data, encrypts this junk data with k, and returns
+-- the resulting encrypted junk data.
 encCrapWKey :: [Char] -> IO [Char];
 encCrapWKey key = generateCrap >>= \crap -> encrypt crap key;
 
