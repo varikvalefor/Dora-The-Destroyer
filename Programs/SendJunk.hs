ff1eb4a90a2cdb6c09d2a93d84fcd6d1406e4007
Varik Valefor <varikvalefor@aol.com>
2021-06-09 21:06:31 -0400

    Programs/SendJunk.hs: Documentation improves.

--- Programs/SendJunk.hs
+++ Programs/SendJunk.hs
@@ -7,6 +7,8 @@ import System.Environment;
 
 -- | For all [String] k, gs k equals a subject line which reflects the
 -- content of k.
+-- gs is used only within SendJunk; all additional relevant information
+-- is available within the definitions of gs.
 gs :: [String] -> String;
 gs a
   | length a > 2 = a !! 2
