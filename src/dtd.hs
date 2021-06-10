febfc5b72669fe50b093f46aa1558494147ecd9a
Varik Valefor <varikvalefor@aol.com>
2021-06-09 21:17:38 -0400

    Some stuff is moved to directory src.

--- src/dtd.hs
+++ src/dtd.hs
@@ -3,6 +3,7 @@ import qualified Programs.SendJunk as SJ;
 import qualified Programs.CrapSum  as CS;
 import qualified Programs.Ten      as TN;
 
+-- | manish determines the program which should be run.
 manish :: String -> IO ();
 manish "sendjunk" = SJ.main;
 manish "ten" = TN.main;
