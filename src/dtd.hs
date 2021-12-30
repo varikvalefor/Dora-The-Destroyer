ecd3839ec3e7890f07ff6f05c4293ef8dfc52f34
Varik Valefor <varikvalefor@aol.com>
2021-12-30 15:59:51 -0500

    main and manish are combined.

--- src/dtd.hs
+++ src/dtd.hs
@@ -1,15 +1,13 @@
+{-# LANGUAGE LambdaCase #-}
+
 import System.Environment;
 import qualified Programs.SendJunk as SJ;
 import qualified Programs.CrapSum  as CS;
 import qualified Programs.Ten      as TN;
 
--- | manish determines the program which should be run.
-manish :: String -> IO ();
-manish "sendjunk" = SJ.main;
-manish "ten" = TN.main;
-manish x
-  | take 7 x == "crapsum" = CS.main;
-manish _ = error "RTFM and stop inputting crap.";
-
 main :: IO ();
-main = getProgName >>= manish;
+main = getProgName >>= \case
+  "sendjunk"                    -> SJ.main
+  "ten"                         -> TN.main
+  foo | take 7 foo == "crapsum" -> CS.main
+  _                             -> error "RTFM and stop entering crap.";
