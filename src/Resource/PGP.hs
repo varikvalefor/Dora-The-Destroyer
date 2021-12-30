bc7b4cda9bb397764a8462fbc3da7bc441f99c19
Varik Valefor <varikvalefor@aol.com>
2021-12-30 16:37:22 -0500

    Documentation is improved.

--- src/Resource/PGP.hs
+++ src/Resource/PGP.hs
@@ -1,6 +1,21 @@
+-- | Module    : Resource.PGP
+-- Description : Encryption rubbish
+-- Copyright   : (c) Varik Valefor, 2021
+-- License     : Unlicense
+-- Maintainer  : varikvalefor@aol.com
+-- Stability   : experimental
+-- Portability : POSIX
+--
+-- This module contains 'encrypt'.
 module Resource.PGP where
 import System.Process;
 
-encrypt :: String -> String -> IO String;
+-- | @encrypt msg rcp@ returns a version of @msg@ which is PGP-encrypted
+-- for the recipient @rcp@.
+encrypt :: String
+        -- ^ The thing which is encrypted
+        -> String
+        -- ^ The name of the recipient
+        -> IO String;
 encrypt message recipient =
   readProcess "gpg2" ["-ear", recipient] message;
